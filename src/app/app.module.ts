eceb2795ba76f664017bf27b58ad9fb7a06e5eb1
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2016-11-06 23:57:13 +0100

    Added routing, nav-bar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { routing } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home.component';
+import { NavBarComponent } from './components/nav-bar.component';
 import { SidePanelComponent } from './components/side-panel.component';
 import { PhotoListComponent } from './components/photo-list.component';
 
@@ -14,13 +17,15 @@ import { PhotoService } from './services/photo.service';
     declarations: [
         AppComponent,
         HomeComponent,
+        NavBarComponent,
         SidePanelComponent,
         PhotoListComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        routing
     ],
     providers: [PhotoService],
     bootstrap: [AppComponent]
